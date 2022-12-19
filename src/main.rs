43ddf6fafabde9c73af5cb59df8c96feb5293eba
Grabowski Łukasz <p2042@PCOSA.AD>
2022-12-19 09:13:51 +0100

    day1

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut output: u64 = 0;
+    let mut counter: u64 = 0;
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip.is_empty() {
+                    if counter > output {
+                        output = counter;
+                    }
+                    counter = 0;
+                    continue;
+                }
+                counter = counter + ip.parse::<u64>().unwrap();
+            } else {
+                println!("someting went bad: {:?}", line);
+            }
+        }
+        println!("{}", output);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
