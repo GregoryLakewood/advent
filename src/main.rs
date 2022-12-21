fbd647aeae56811b359c317a507a48ae33af64b2
Grabowski Łukasz <p2042@PCOSA.AD>
2022-12-21 08:04:21 +0100

    day2

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,33 @@ use std::io::{self, BufRead, Write};
 use std::path::Path;
 
 fn main() {
-    let mut output = vec![0, 0, 0_u64];
-    let mut counter: u64 = 0;
+    let mut output = 0_u64;
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                if ip.is_empty() {
-                    for value in output.iter().enumerate() {
-                        if counter > *value.1 {
-                            output.insert(value.0, counter);
-                            output.pop();
-                            break;
-                        }
-                    }
-                    counter = 0;
-                    continue;
+                match ip.as_str() {
+                    "A X" => output += 4,
+                    "B X" => output += 1,
+                    "C X" => output += 7,
+                    "A Y" => output += 8,
+                    "B Y" => output += 5,
+                    "C Y" => output += 2,
+                    "A Z" => output += 3,
+                    "B Z" => output += 9,
+                    "C Z" => output += 6,
+                    "" => break,
+                    _ => panic!(),
                 }
-                counter = counter + ip.parse::<u64>().unwrap();
+
             } else {
                 println!("someting went bad: {:?}", line);
             }
         }
         println!("{:?}", output);
-        println!("{}", output.iter().sum::<u64>());
         if let Ok(mut file) = File::create("output.txt") {
-            file.write(output.iter().sum::<u64>().to_string().as_bytes())
+            file.write(output.to_string().as_bytes())
                 .unwrap();
         }
     }
